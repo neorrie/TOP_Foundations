8e2077cf71b21a9951a4f794ac6f717738b67f64
neorrie <gappaneo@gmail.com>
2025-03-24 22:21:30 +0800

    done!

--- test.js
+++ test.js
@@ -1,7 +1,30 @@
 const container = document.querySelector("#container");
 
-for (i = 0; i < 256; i++) {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("box");
-  container.appendChild(newDiv);
+function createGrid(gridSize = 16) {
+  container.innerHTML = "";
+  for (i = 0; i < gridSize * gridSize; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("box");
+    container.appendChild(newDiv);
+  }
 }
+
+createGrid();
+
+function addHover() {
+  document.querySelectorAll(".box").forEach((box) => {
+    box.addEventListener("mouseover", () => {
+      box.classList.add("hovered");
+    });
+  });
+}
+
+addHover();
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  let newGridSize = parseInt(prompt("Enter a number"));
+  document.documentElement.style.setProperty("--sides", newGridSize);
+  createGrid(newGridSize);
+  addHover();
+});
