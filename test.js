3d7e6efd8158dc8bbb23059ada0f80705eef677d
gappaneo <gabrielneo.2021@scis.smu.edu.sg>
2025-03-24 17:02:16 +0800

    help

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container");
 
-for (i = 0; i < 16; i++) {
+for (i = 0; i < 256; i++) {
   const newDiv = document.createElement("div");
   newDiv.classList.add("box");
   container.appendChild(newDiv);
